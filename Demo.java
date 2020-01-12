689474328c790386f4e81f9fe01a8b362ec753b6
louxiaoxin <790470307@qq.com>
2020-01-12 09:04:18 +0800

    add 循环队列

--- Demo.java
+++ Demo.java
@@ -1,11 +1,12 @@
 package Algorithm_lou;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class Demo {
     public static void main(String[] args) {
         List<String> tmp = Arrays.asList("fa", "df");
+        Set<int[]> set = new HashSet<>();
+        Iterator<int[]> ite = set.iterator();
         System.out.println(tmp);
     }
 }
